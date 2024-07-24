200e6d030661b773aa478c184071b16fa27905df
AlexBabilya <djesssash1708@gmail.com>
2024-07-24 12:27:40 +0300

    Hey, mambo! Mambo Italiano

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export MODULAR_HOME="/Users/oleksandrbabilia/.modular"
+export PATH="/Users/oleksandrbabilia/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp" , "prettier", "gopls",
+        "csharpier", "netcoredbg", "omnisharp",
+      },
+    },
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+  --
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
